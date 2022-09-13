c0e5c2b24f5b725cfc5f477a82061e6e0ddbe4c0
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-09-13 15:17:05 -0700

    added most actions and functions, occasionally working

--- app.js
+++ app.js
@@ -4,13 +4,66 @@
 
 /* Actions */
 function loadPage() {}
+function compareInputs(pInput, cInput) {
+    const currentMatch = `${pInput} vs ${cInput}`;
+
+    // Tie check
+    if (pInput === cInput) {
+        alert(`${currentMatch} is a Tie`);
+        return;
+    }
+
+    // Rock
+    if (pInput === "Rock") {
+        if (cInput === "Scissors") {
+        alert(`${currentMatch} = You Win`);
+        } else {
+        alert(`${currentMatch} = Computer Wins`);
+        }
+    }
+    // Paper
+    else if (pInput === "Paper") {
+        if (cInput === "Rock") {
+            alert(`${currentMatch} = You Win`);
+        } else {
+            alert(`${currentMatch} = Computer Wins`);
+        }
+        }
+      // Scissors
+        else if (pInput === "Scissors") {
+        if (cInput === "Paper") {
+            alert(`${currentMatch} = You Win`);
+        } else {
+            alert(`${currentMatch} = Computer Wins`);
+        }
+    }
+    function updateMoves(pInput, cInput) {
+        document.getElementById("p-move").src = `./assets/${pInput}.svg`;
+        document.getElementById("c-move").src = `./assets/${cInput}.svg`;
+    }    
+    
+
 
 /* Components */
+const options = document.querySelectorAll(".options");
+
+options.forEach((option) => {
+    option.addEventListener("click", function () {
+        const pInput = this.value;
+
+        const cOptions = ["Rock", "Paper", "Scissors"];
+        const cInput = cOptions[Math.floor(Math.random() * 3)];
+        updateMoves(pInput, cInput)
+        compareInputs(pInput, cInput);
+    });
+}
 
 /* Component */
 // get DOM
 // display
 // event listeners
 
+
+
 /* Run page load code */
-loadPage();
+loadPage()
