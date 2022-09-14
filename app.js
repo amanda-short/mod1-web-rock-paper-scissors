e01d68be872172ef2bf596e65ef9ab92cfa9647b
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-09-13 21:13:49 -0700

    repaired all button connections

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* Imports */
-// import { getRandomItem, score } from '.utils.js';
+import { getRandomItem, getRandomNumber } from '/utils.js';
 
 let gameState = 'guess'; //guess, results
 let guess = ''; //rock, paper, scissors
@@ -8,15 +8,16 @@ let result = '';
 let wins = 0;
 let losses = 0;
 
-const options = ['Rock', 'Paper', 'Scissors']
+const options = ['rock', 'paper', 'scissors'];
 
-const Rock = document.getElementById('choose-rock');
-const Paper = document.getElementById('choose-paper');
-const Scissors = document.getElementById('choose-scissors');
+const rock = document.getElementById('choose-rock');
+const paper = document.getElementById('choose-paper');
+const scissors = document.getElementById('choose-scissors');
 
 const rockButton = document.getElementById('rock-button');
 const paperButton = document.getElementById('paper-button');
 const scissorsButton = document.getElementById('scissors-button');
+const playAgainButton = document.getElementById('play-again-button');
 
 
 //functions
@@ -29,18 +30,20 @@ function loadPage() {
 //events
 rockButton.addEventListener('click', () => {
     handleFight('rock');
+
+   
 });
 paperButton.addEventListener('click', () => {
     handleFight('paper');
 });
-rockButton.addEventListener('click', () => {
+scissorsButton.addEventListener('click', () => {
     handleFight('scissors');
 });
 
 //actions
 function handleFight(pick) {
     computer = getRandomItem(guess);
-    const result = score(pick, computer);
+    const result = getRandomNumber(pick, computer);
 
     if (result === 1) {
         wins++;
@@ -57,13 +60,14 @@ function handleFight(pick) {
 }
 
 function playAgain() {
-    gameState;
+    gameState = 'guess';
     loadPage();
 }
 
 playAgainButton.addEventListener('click', () => {
     playAgain();
-});
+}
+);
 
 
 /* Run page load code */
